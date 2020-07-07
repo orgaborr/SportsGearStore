d1bbc78fdd7dff31cbecde8675955179556afee3
Gabor Orban <orbangabeszb@gmail.com>
2020-07-07 12:23:36 +0200

    StockDaoImpl class added

---
+++ src/main/java/com/orgabor/sportsgearstore/StockDaoImpl.java
@@ -0,0 +1,40 @@
+package com.orgabor.sportsgearstore;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StockDaoImpl implements StockDao {
+	
+	private List<Product> products = new LinkedList<>();
+
+	public void addProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Product getProduct(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Product> retrieveByCategory(String category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Product> retrieveByExpression(String expression) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateProduct(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteProduct(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
