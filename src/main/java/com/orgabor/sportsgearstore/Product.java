14374c95745b7f39baee6471b4eb3b3c8bd526a8
Gabor Orban <orbangabeszb@gmail.com>
2020-07-07 12:05:20 +0200

    Product class added with fields

---
+++ src/main/java/com/orgabor/sportsgearstore/Product.java
@@ -0,0 +1,10 @@
+package com.orgabor.sportsgearstore;
+
+public class Product {
+	private int id;
+	private String name;
+	private String description;
+	private String category;
+	private double price;
+	private int inStock;
+}
