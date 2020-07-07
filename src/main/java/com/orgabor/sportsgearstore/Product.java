21607fa21b94c4b090e615c984f81c2f705271cc
Gabor Orban <orbangabeszb@gmail.com>
2020-07-07 12:06:51 +0200

    Product constructor, setters and getters added

--- src/main/java/com/orgabor/sportsgearstore/Product.java
+++ src/main/java/com/orgabor/sportsgearstore/Product.java
@@ -7,4 +7,63 @@ public class Product {
 	private String category;
 	private double price;
 	private int inStock;
+	
+	public Product(int id, String name, String description, String category, double price, int inStock) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.category = category;
+		this.price = price;
+		this.inStock = inStock;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getInStock() {
+		return inStock;
+	}
+
+	public void setInStock(int inStock) {
+		this.inStock = inStock;
+	}
+	
 }
