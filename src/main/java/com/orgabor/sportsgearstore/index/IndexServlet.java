b1a573723cf4b761b3846960d0ed44120f45b6d6
Gabor Orban <orbangabeszb@gmail.com>
2020-07-15 21:19:00 +0200

    refresh bootstrap crash fixed

--- src/main/java/com/orgabor/sportsgearstore/index/IndexServlet.java
+++ src/main/java/com/orgabor/sportsgearstore/index/IndexServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/index")
 public class IndexServlet extends HttpServlet {
 
 	@Override
