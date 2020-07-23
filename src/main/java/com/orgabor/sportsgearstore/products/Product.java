dae008181635e4f8fca12cb949a4af38f9c34117
Gabor Orban <orbangabeszb@gmail.com>
2020-07-23 19:35:57 +0200

    Product made immutable

--- src/main/java/com/orgabor/sportsgearstore/products/Product.java
+++ src/main/java/com/orgabor/sportsgearstore/products/Product.java
@@ -2,11 +2,11 @@ package com.orgabor.sportsgearstore.products;
 
 public class Product {
 	private final int id;
-	private String name;
-	private String description;
-	private Categories category;
-	private double price;
-	private int inStock;
+	private final String name;
+	private final String description;
+	private final Categories category;
+	private final double price;
+	private final int inStock;
 	
 	public Product(int id, String name, String description, Categories category, double price, int inStock) {
 		this.id = id;
@@ -25,40 +25,19 @@ public class Product {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getDescription() {
 		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public Categories getCategory() {
 		return category;
 	}
 
-	public void setCategory(Categories category) {
-		this.category = category;
-	}
-
 	public double getPrice() {
 		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
 	}
 
 	public int getInStock() {
 		return inStock;
 	}
-
-	public void setInStock(int inStock) {
-		this.inStock = inStock;
-	}
-	
 }
