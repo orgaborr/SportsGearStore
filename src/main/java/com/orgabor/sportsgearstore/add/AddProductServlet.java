91ab0394bf3ce4d03f01a1bf9da8acddf546e71b
Gabor Orban <orbangabeszb@gmail.com>
2020-07-24 19:39:21 +0200

    AddProductServlet added

---
+++ src/main/java/com/orgabor/sportsgearstore/add/AddProductServlet.java
@@ -0,0 +1,9 @@
+package com.orgabor.sportsgearstore.add;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/add-product.do")
+public class AddProductServlet extends HttpServlet {
+	
+}
