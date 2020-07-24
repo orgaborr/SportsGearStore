1106eb0a2e8c91a85cb4bcb20550038ac8d2ec30
Gabor Orban <orbangabeszb@gmail.com>
2020-07-24 18:42:48 +0200

    DeleteProductServlet added1

---
+++ src/main/java/com/orgabor/sportsgearstore/delete/DeleteProductServlet.java
@@ -0,0 +1,9 @@
+package com.orgabor.sportsgearstore.delete;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/delete-product.do")
+public class DeleteProductServlet extends HttpServlet{
+	
+}
